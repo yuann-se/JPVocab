64cb4fb183ff8ddb472cfeda80d65583f698280b
yuann-se <103625509+yuann-se@users.noreply.github.com>
2022-09-06 15:21:22 +0300

    Initial environment setup.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+</style>
